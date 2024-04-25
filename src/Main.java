088323ebbc778509ed9b39c3dd04a9f1f6a40dcf
AleksandraSkrypnik <skrypnik-ae@mail.ru>
2024-04-25 17:35:38 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int amount = 400;
-        int amountDeposit = 2400;
-        int bonuses = amountDeposit / 100;
+        int amount = 600;
+        int amountDeposit = 3400;
+        int bonuses = amountDeposit > 1000 ? amountDeposit / 100 : 0;
         int totalAmount = amount + amountDeposit + bonuses;
 
-        if (amountDeposit >= 100){
-            bonuses = bonuses;
-        }
         System.out.println(" Бонус равен " + bonuses + " рублям ");
         System.out.println(" Итоговая сумма на счету " + totalAmount + " рублей ");
     }
